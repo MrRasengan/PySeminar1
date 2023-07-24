2915d9dac311d128d9e32c0cee8aa6b4e892def3
Konstantin <kostik-kostik-12@mail.ru>
2023-07-24 15:22:08 +0300

    Pull

---
+++ start.py
@@ -0,0 +1,16 @@
+"""
+speed = int(input("Введите скорость "))
+time = float(input("Введите время "))
+distance = speed * time
+print("Ваш автомобиль прошел", distance, "киллометров")
+"""
+
+'''
+num = int(input("Введите число "))
+if num > 0:
+    print(f "Число {num} положительное")
+elif num == 0:
+    print("Это ноль")
+else:
+    print("Число отрицательное")
+'''
